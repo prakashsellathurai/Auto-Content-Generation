81dba208d89957e051994d2912b07a2138ebc94c
prakash <prakashsellathurai@gmail.com>
2020-09-17 18:35:34 +0530

    updated test script

--- run_tests.js
+++ run_tests.js
@@ -1,5 +1,21 @@
 
 const jasmine_util = require('@tensorflow/tfjs-core/dist/jasmine_util');
-const runTests = require('../test_util').runTests;
+function runTests(jasmineUtil, specFiles) {
+    // tslint:disable-next-line:no-require-imports
+    const jasmineConstructor = require('jasmine');
+  
+    Error.stackTraceLimit = Infinity;
+  
+    process.on('unhandledRejection', e => {
+      throw e;
+    });
+  
+    jasmineUtil.setTestEnvs(
+        [{name: 'node', factory: jasmineUtil.CPU_FACTORY, features: {}}]);
+  
+    const runner = new jasmineConstructor();
+    runner.loadConfig({spec_files: specFiles, random: false});
+    runner.execute();
+  }
 
 runTests(jasmine_util, ['./*test.js']);
